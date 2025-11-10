1c607f8c0e0ff0a640be1ad1be3104f74e547ac7
GiandomenicoIameo <g.iameo@studenti.unina.it>
2025-11-11 00:06:18 +0100

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ import org.example.gui.Login;
 
 public class Main {
     public static void main(String[] args) {
-        Login var = new Login();
-        var.setVisible(true);
+        Login login = new Login();
+        login.setVisible(true);
     }
 }
